7d0b2b7289701df3a77d37c8d295380abb8983d9
himanshu <himanshugharat44@gmail.com>
2020-10-23 10:17:22 -0500

    add: the validation for the registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 import { LoginComponent } from './components/login/login.component';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,6 +28,9 @@ import { LoginComponent } from './components/login/login.component';
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AbstractControl, FormBuilder, FormGroup, Validators } from "@angular/forms"
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,13 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RegisterComponent implements OnInit {
-  
-  constructor() { }
-
-  ngOnInit(): void {
-   
-
+  hide = true;
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      firstFormControl: ["", 
+        Validators.pattern('[a-zA-Z]{2,}')],
+      lastFormControl: ["", 
+        Validators.pattern('[a-zA-Z]{2,}')],
+      emailFormControl: ["", 
+        Validators.email],
+      passFormControl: ["", 
+        Validators.pattern('[A-Za-z0-9\\d!$%@#£€*?&]{8,}$')],
+      passConfirmFormControl: ["", Validatepass],
+    })
+    function Validatepass(control: AbstractControl) {
+      if (control && (control.value != null || control.value != undefined)) {
+        const passConfirmValue = control.value;
+        const passControl = control.root.get('passFormControl')
+        if (passControl) {
+          const passValue = passControl.value;
+          if (passValue !== passConfirmValue) {
+            return {
+              isError: true
+            }
+          }
+        }
+      }
+    }
   }
-
-
+  onubmit() {
+    console.log(this.form.value)
+  }
+  ngOnInit(): void {
+  }
 }
