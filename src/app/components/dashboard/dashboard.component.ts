ca29b23080cf8586a2305f4109515e58a2b107a5
himanshu <himanshugharat44@gmail.com>
2020-10-28 08:00:53 -0500

    fix: the dashboard component the hover effect

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   showFiller = false;
+  isButtonVisible = true;
   typesOfOperation: string[] = ['Note', 'Reminder', 'Edit lable', 'Archive', 'Bin'];
   constructor() { }
 
