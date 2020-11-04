692b605b58103b1e3237c2949a0b4b708ac0c187
himanshu <himanshugharat44@gmail.com>
2020-11-04 22:46:08 +0530

    add: the route of bin to the dashboard

--- src/app/service/notes.service.ts
+++ src/app/service/notes.service.ts
@@ -10,24 +10,24 @@ export class NotesService {
   constructor(private http: HttpService) { }
 
   addNotes(data) {
-    let url = this._url+"addNotes"
-    return this.http.postService(data,url,true ,{headers:{'Authorization':localStorage.getItem('token')}})
+    let url = this._url + "addNotes"
+    return this.http.postService(data, url, true, { headers: { 'Authorization': localStorage.getItem('token') } })
   }
   getNotes() {
-    let url = this._url+"getNotesList"
-    return this.http.getService(url,true ,{headers:{'Authorization':localStorage.getItem('token')}})
+    let url = this._url + "getNotesList"
+    return this.http.getService(url, true, { headers: { 'Authorization': localStorage.getItem('token') } })
   }
-  updateNote(data){
-    let url = this._url+"updateNotes"
-    return this.http.postService(data,url,true ,{headers:{'Authorization':localStorage.getItem('token')}})
+  updateNote(data) {
+    let url = this._url + "updateNotes"
+    return this.http.postService(data, url, true, { headers: { 'Authorization': localStorage.getItem('token') } })
   }
-  deleteNote(data){
-    let url = this._url+"trashNotes"
-    return this.http.postService(data,url,true ,{headers:{'Authorization':localStorage.getItem('token')}})
+  deleteNote(data) {
+    let url = this._url + "trashNotes"
+    return this.http.postService(data, url, true, { headers: { 'Authorization': localStorage.getItem('token') } })
   }
-  getTrashNote(){
-    let url = this._url+"getTrashNotesList"
-    return this.http.getService(url,true ,{headers:{'Authorization':localStorage.getItem('token')}})
+  getTrashNote() {
+    let url = this._url + "getTrashNotesList"
+    return this.http.getService(url, true, { headers: { 'Authorization': localStorage.getItem('token') } })
   }
 }
 
